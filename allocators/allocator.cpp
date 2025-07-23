b7b147dd05521d52119feb65a2851d56814119d6
mplata <mariusz.plata@volvocars.com>
2025-07-23 07:39:26 +0200

    Added second exercise - naive allocator

---
+++ allocators/allocator.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <new>
+#include <vector>
+
+// Test classes
+class A {
+  int x;
+
+ public:
+  A() : x(1) { std::cout << "A constructed\n"; }
+  ~A() { std::cout << "A destructed\n"; }
+};
+
+class MyAllocator {
+ public:
+  void* allocate(std::size_t size) {
+    std::cout << "[MyAllocator] Allocated " << size << " bytes at "
+              << static_cast<void*>(buffer + offset) << std::endl;
+    offset += size;
+    return buffer + offset - size;
+  }
+
+  void deallocate(void* ptr) {
+    std::cout << "[MyAllocator] Deleting memory at " << ptr << std::endl;
+  }
+
+ private:
+  int offset = 0;
+  static unsigned char buffer[256];
+};
+
+unsigned char MyAllocator::buffer[256] = {0};
+
+MyAllocator alloc;
+
+// 1. Overload global operator new/delete here (print pointer and size)
+void* operator new(std::size_t size) {
+  void* ptr = alloc.allocate(size);
+  if (!ptr) throw std::bad_alloc();
+  return ptr;
+}
+void operator delete(void* ptr) noexcept { alloc.deallocate(ptr); }
+
+int main() {
+  std::cout << "--- Allocating/Deallocating A ---\n";
+  A* a = new A;
+  delete a;
+
+  // 6. Play with std::vector<int> (insert some elements, so how allocations are
+  // working)
+  std::cout << "--- std::vector<int> allocation ---\n";
+  std::vector<int> v;
+  for (int i = 0; i < 3; ++i) v.push_back(i);
+
+  return 0;
+}
